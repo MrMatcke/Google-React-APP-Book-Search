5d885a65fd7810739199c31cd2864f5851784042
Mike Matcke <mr.matcke@gmail.com>
2021-05-10 20:17:03 -0700

    updating server.js

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
+//Techonology needed to begin and start app
 const express = require("express");
 const app = express();
 const routes = require("./routes")
-const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3001;
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
+//For connecting MongoDB
+const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactgooglebooks");
 
+
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  }
-  app.use(routes);
+  app.use(express.static("client/build"));
+}
 
+// Add routes, both API and view
+app.use(routes);
 
-  app.listen(PORT, function() {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-  });
+const PORT = process.env.PORT || 3001;
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
